b40a4834ca2bb32dbcf4c34772b9a063a973806b
Drew Wobeck <dwobeck@gmail.com>
2018-08-15 16:53:27 +0000

    GET and POST paths successfully connected, some scss

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,49 @@
 import React, { Component } from 'react';
 import fetch from 'node-fetch';
 
-  const GalleryBody = fetch('https://stark-meadow-71570.herokuapp.com/doggo')
-    .then(res => res.json())
-    .then(json => console.log(json));
+
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      data: null
+    }
     this.handleNav = this.handleNav.bind(this);
+    this.handleVote = this.handleVote.bind(this);
   }
   handleNav(event) {
     event.preventDefault();
     this.props.history.push('/add-doggo');
   }
-
+  handleVote(id) {
+  /*  fetch('https://stark-meadow-71570.herokuapp.com/doggo', {method: "PUT", mode: "cors", credentials: 'omit', headers: {"Content-Type": "application/json; charset=utf-8"}, body: JSON.stringify({"_id": id})
+    })*/
+    console.log(id);
+  }
+  componentWillMount() {
+  fetch('https://stark-meadow-71570.herokuapp.com/doggo', {mode: "cors"})
+    .then(res => res.json())
+    .then(json => this.setState({data: json}));    
+  }
   
   render() {
+    if(!this.state.data) {
+      return (<div />)
+    }
+    
     return (
         <div className="app">
           <div className="galleryHead">
             <h2> Today's Doggos </h2>
           </div>
-          <div className="GalleryBody">
-            {GalleryBody.map((doggo) => {
+          <div className="galleryBody">
+            {this.state.data.map((doggo) => {
               return (
                 <div className="galleryImg" key={doggo._id}>
                   <img src={doggo.link} alt="img" />
                   <p> Votes: {doggo.meta.votes} </p>
-                  <p> {doggo.date.slice(0, 9)} </p>
+                  <p> {doggo.date.slice(0, 10)} </p>
+                  <button onClick={this.handleVote(doggo._id)}>vote</button>
                 </div>
               )
             })}
--- client/src/addDoggo.js
+++ client/src/addDoggo.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
 
 class AddDoggo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            link: ''
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(event) {
+        const target = event.target;
+        const value = target.value;
+        this.setState({link: value});
+    }
+    handleSubmit(event) {
+        event.preventDefault();
+        let link = this.state.link;
+        fetch('https://stark-meadow-71570.herokuapp.com/doggo', {method: "POST", mode: "cors", credentials: "omit", headers: {"Content-Type": "application/json; charset=utf-8"}, body: JSON.stringify({"link": link})});
+    }
     render() {
         return (
             <div>
@@ -8,8 +26,8 @@ class AddDoggo extends Component {
                     <div className="formLabel">
                         Add a Doggo
                     </div>
-                    <input type="text" placeholder="link" />
-                    <button type="submit"> Submit </button>
+                    <input onChange={this.handleChange} type="text" placeholder="link" />
+                    <button onClick={this.handleSubmit}> Submit </button>
                 </form>
             </div>
             )
